137707e12ab6b3c7254f380f7b3ec72b3c790616
Eidi Ueno <eidhie@gmail.com>
2021-12-07 16:18:32 +0900

    Add Day2

--- util/util.py
+++ util/util.py
@@ -1,8 +1,12 @@
 import sys
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, Optional
 
 T = TypeVar("T")
 
 
+def echo(arg: T) -> T:
+    return arg
+
+
 def read_from_stdin_as_array(cast_func: Callable[[str], T]) -> list[T]:
-    return [cast_func(line) for line in sys.stdin]
+    return [cast_func(line.strip()) for line in sys.stdin]
