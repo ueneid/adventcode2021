195785af4ba030c985db28315da23581730dd61f
Eidi Ueno <eidhie@gmail.com>
2021-12-08 19:11:38 +0900

    Add Day3 part2

--- day3/answer.py
+++ day3/answer.py
@@ -15,8 +15,50 @@ def answer1(lines):
     return gamma * epsilon
 
 
-def answer2(lines):
-    pass
+def divide_bit_list(bits: list[str], index: int) -> tuple[list[str], list[str]]:
+    bit0, bit1 = [], []
+    for bit in bits:
+        if bit[index] == "0":
+            bit0.append(bit)
+        else:
+            bit1.append(bit)
+    return bit0, bit1
+
+
+def get_oxygen_generator_rating(lines: list[str]) -> int:
+    bits = lines
+    index = 0
+    while bits:
+        if len(bits) == 1:
+            break
+        bit0, bit1 = divide_bit_list(bits, index)
+        len_bit0, len_bit1 = len(bit0), len(bit1)
+        if len_bit0 <= len_bit1:
+            bits = bit1
+        else:
+            bits = bit0
+        index += 1
+    return int("0b" + bits[0], 2)
+
+
+def get_CO2_scrubber_rating(lines: list[str]) -> int:
+    bits = lines
+    index = 0
+    while bits:
+        if len(bits) == 1:
+            break
+        bit0, bit1 = divide_bit_list(bits, index)
+        len_bit0, len_bit1 = len(bit0), len(bit1)
+        if len_bit0 > len_bit1:
+            bits = bit1
+        else:
+            bits = bit0
+        index += 1
+    return int("0b" + bits[0], 2)
+
+
+def answer2(lines) -> int:
+    return get_oxygen_generator_rating(lines) * get_CO2_scrubber_rating(lines)
 
 
 if __name__ == "__main__":
