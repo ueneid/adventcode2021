659e27f5fadc4eb8502a759740c25f818b20dfb0
Eidi Ueno <eidhie@gmail.com>
2021-12-14 17:19:38 +0900

    Add the answer for the part1 of day5

--- day5/answer.py
+++ day5/answer.py
@@ -6,15 +6,72 @@ sys.path.append(str(current_dir) + '/../')
 from util.util import read_lines_from_stdin, echo
 
 
-def answer1(lines):
-    pass
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
 
+    def __repr__(self) -> str:
+        return "x: {}, y: {}\n".format(self.x, self.y)
+
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point) -> None:
+        self.p1 = p1
+        self.p2 = p2
+
+    def is_horizontal(self) -> bool:
+        return self.p1.y == self.p2.y
+
+    def is_vertical(self) -> bool:
+        return self.p1.x == self.p2.x
+
+    def get_all_points(self) -> list[Point]:
+        points: list[Point] = []
+        for x in range(min(self.p1.x, self.p2.x), max(self.p1.x, self.p2.x) + 1):
+            for y in range(min(self.p1.y, self.p2.y), max(self.p1.y, self.p2.y) + 1):
+                points.append(Point(x, y))
+        return points
+
+    def __repr__(self) -> str:
+        return "({}, {}) -> ({}, {})".format(self.p1.x, self.p1.y, self.p2.x, self.p2.y)
+
+
+def answer1(vent_lines: list[Line]) -> int:
+    diagram: dict[Point, int] = {}
+    for line in vent_lines:
+        if not (line.is_horizontal() or line.is_vertical()):
+            continue
+        for point in line.get_all_points():
+            if point in diagram:
+                diagram[point] += 1
+            else:
+                diagram[point] = 1
+    return len([v for v in diagram.values() if v > 1])
 
 def answer2(lines):
     pass
 
 
 if __name__ == "__main__":
-    lines = read_lines_from_stdin(echo)
-    print(answer1(lines))
-    print(answer2(lines))
+    def helper(lines: list[str]) -> list[Line]:
+        ret: list[Line] = []
+        for line in lines:
+            points: list[Point] = []
+            for start_end_points in line.split(" -> "):
+                x, y = [p for p in start_end_points.split(",")]
+                points.append(Point(int(x), int(y)))
+            ret.append(Line(*points))
+            points = []
+        return ret
+    vent_lines = read_lines_from_stdin(helper)
+    print(answer1(vent_lines))
+    print(answer2(vent_lines))
